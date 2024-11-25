c9e0a9be5203c18172dfa6aeb71c00dd396d440f
Anton Saph1s Babenko <saph1s@cyberzone.dev>
2024-11-26 00:22:42 +0300

    update: move enum to .cs file + new UserRequest model

--- AuthentikNet.Api/Models/UserRequest.cs
+++ AuthentikNet.Api/Models/UserRequest.cs
@@ -2,21 +2,15 @@
 
 namespace AuthentikNet.Api.Models;
 
-public class User
+public class UserRequest
 {
-    [JsonPropertyName("pk")] public required int Pk { get; init; }
     [JsonPropertyName("username")] public required string Username { get; set; }
     [JsonPropertyName("name")] public required string Name { get; set; }
     [JsonPropertyName("is_active")] public bool IsActive { get; set; }
     [JsonPropertyName("last_login")] public DateTime? LastLogin { get; set; }
-    [JsonPropertyName("is_superuser")] public required bool IsSuperUser { get; init; }
     [JsonPropertyName("groups")] public List<Guid> Groups { get; set; } = [];
-    [JsonPropertyName("groups_obj")] public required List<UserGroup> GroupsObj { get; init; }
     [JsonPropertyName("email")] public string Email { get; set; } = string.Empty;
-    [JsonPropertyName("avatar")] public required string Avatar { get; init; }
     [JsonPropertyName("attributes")] public Dictionary<string, string> Attributes { get; set; } = new();
-    [JsonPropertyName("uid")] public required string Uid { get; init; }
     [JsonPropertyName("path")] public string Path { get; set; } = string.Empty;
     [JsonPropertyName("type")] public UserTypeEnum Type { get; set; }
-    [JsonPropertyName("uuid")] public required Guid Uuid { get; init; }
 }
---
+++ AuthentikNet.Api/Models/UserTypeEnum.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace AuthentikNet.Api.Models;
+
+/// <summary>
+/// The type of account
+/// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum UserTypeEnum
+{
+    /// <summary>
+    /// Internal
+    /// </summary>
+    [EnumMember(Value = "internal")] Internal,
+
+    /// <summary>
+    /// External
+    /// </summary>
+    [EnumMember(Value = "external")] External,
+
+    /// <summary>
+    /// Service account
+    /// </summary>
+    [EnumMember(Value = "service_account")]
+    ServiceAccount,
+
+    /// <summary>
+    /// Internal service account
+    /// </summary>
+    [EnumMember(Value = "internal_service_account")]
+    InternalServiceAccount
+}
