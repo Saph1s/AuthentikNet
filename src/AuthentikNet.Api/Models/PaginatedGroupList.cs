bb4cd2a3d054f5f807067adc02022f48273d13c9
Anton "Saph1s" Babenko <saph1s@cyberzone.dev>
2025-06-11 00:42:54 +0300

    update: add PaginatedGroupList model for handling paginated group data

---
+++ src/AuthentikNet.Api/Models/PaginatedGroupList.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AuthentikNet.Api.Models;
+
+public class PaginatedGroupList
+{
+    [JsonPropertyName("pagination")] public required Pagination Pagination { get; set; }
+    [JsonPropertyName("results")] public required List<Group> Results { get; set; }
+}
