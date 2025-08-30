6316576ed03ccb19a1497055a52e13cc751e3e99
Anton "Saph1s" Babenko <saph1s@cyberzone.dev>
2025-08-30 22:38:17 +0300

    update: add JsonIgnore attribute to nullable properties in PatchedUserRequest and PatchedSettingsRequest

--- src/AuthentikNet.Api/Models/PatchedSettingsRequest.cs
+++ src/AuthentikNet.Api/Models/PatchedSettingsRequest.cs
@@ -13,78 +13,91 @@ public class PatchedSettingsRequest
     /// Configure how authentik should show avatars for users.
     /// </summary>
     [JsonPropertyName("avatars")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string? Avatars { get; set; }
 
     /// <summary>
     /// Enable the ability for users to change their name.
     /// </summary>
     [JsonPropertyName("default_user_change_name")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? DefaultUserChangeName { get; set; }
 
     /// <summary>
     /// Enable the ability for users to change their email address.
     /// </summary>
     [JsonPropertyName("default_user_change_email")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? DefaultUserChangeEmail { get; set; }
 
     /// <summary>
     /// Enable the ability for users to change their username.
     /// </summary>
     [JsonPropertyName("default_user_change_username")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? DefaultUserChangeUsername { get; set; }
 
     /// <summary>
     /// Events will be deleted after this duration.(Format: weeks=3;days=2;hours=3,seconds=2).
     /// </summary>
     [JsonPropertyName("event_retention")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string? EventRetention { get; set; }
 
     /// <summary>
     /// Reputation cannot decrease lower than this value. Zero or negative.
     /// </summary>
     [JsonPropertyName("reputation_lower_limit")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public int? ReputationLowerLimit { get; set; }
 
     /// <summary>
     /// Reputation cannot increase higher than this value. Zero or positive.
     /// </summary>
     [JsonPropertyName("reputation_upper_limit")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public int? ReputationUpperLimit { get; set; }
 
     /// <summary>
     /// The option configures the footer links on the flow executor pages.
     /// </summary>
     [JsonPropertyName("footer_links")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<(string href, string name)>? FooterLinks { get; set; } = [];
 
     /// <summary>
     /// When enabled, all the events caused by a user will be deleted upon the user's deletion.
     /// </summary>
     [JsonPropertyName("gdpr_compliance")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? GdprCompliance { get; set; }
 
     /// <summary>
     /// Globally enable/disable impersonation.
     /// </summary>
     [JsonPropertyName("impersonation")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? Impersonation { get; set; }
 
     /// <summary>
     /// Require administrators to provide a reason for impersonating a user.
     /// </summary>
     [JsonPropertyName("impersonation_require_reason")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? ImpersonationRequireReason { get; set; }
 
     /// <summary>
     /// Default token duration
     /// </summary>
     [JsonPropertyName("default_token_duration")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string? DefaultTokenDuration { get; set; }
 
     /// <summary>
     /// Default token length
     /// </summary>
     [JsonPropertyName("default_token_length")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public int? DefaultTokenLength
     {
         get => _defaultTokenLength;
--- src/AuthentikNet.Api/Models/PatchedUserRequest.cs
+++ src/AuthentikNet.Api/Models/PatchedUserRequest.cs
@@ -5,17 +5,40 @@ namespace AuthentikNet.Api.Models;
 
 public class PatchedUserRequest
 {
-    [JsonPropertyName("username")] public string? Username { get; set; }
-    [JsonPropertyName("name")] public string? Name { get; set; }
-    [JsonPropertyName("is_active")] public bool? IsActive { get; set; }
-    [JsonPropertyName("last_login")] public DateTime? LastLogin { get; set; }
-    [JsonPropertyName("groups")] public List<Guid>? Groups { get; set; }
-    [JsonPropertyName("email")] public string? Email { get; set; }
+    [JsonPropertyName("username")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Username { get; set; }
+
+    [JsonPropertyName("name")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("is_active")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? IsActive { get; set; }
+
+    [JsonPropertyName("last_login")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public DateTime? LastLogin { get; set; }
+
+    [JsonPropertyName("groups")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public List<Guid>? Groups { get; set; }
+
+    [JsonPropertyName("email")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Email { get; set; }
 
     [JsonPropertyName("attributes")]
     [JsonConverter(typeof(DynamicAttributesJsonConverter))]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public object? Attributes { get; set; }
 
-    [JsonPropertyName("path")] public string? Path { get; set; }
-    [JsonPropertyName("type")] public UserTypeEnum? Type { get; set; }
+    [JsonPropertyName("path")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Path { get; set; }
+
+    [JsonPropertyName("type")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public UserTypeEnum? Type { get; set; }
 }
